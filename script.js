32658c12e30bdec9dc44b6d21d7d3d94c11cbd54
mohsub <39135772+mohsub@users.noreply.github.com>
2018-06-25 17:37:15 +0200

    initial

---
+++ script.js
@@ -0,0 +1,33 @@
+const carMakes = ['Honda','BMW','Skoda','Volvo','Fiat'];
+
+const carColors = ['lightgrey', 'lightcyan','lightyellow','lightgreen','lightcoral','lightpink'];
+
+let cars = [];
+
+function cargenerator(numberOfcars){
+
+let numberOfcars = 10;
+
+for( i = 0; i < 10; i++){
+
+    let car = {};
+
+    const carmakerindex = randomIntFromInterval(0,carMakes.length-1);
+
+    const carcolorindex = randomIntFromInterval(0,carColors.length-1);
+
+   car.make = carMakes[armakerindex];
+
+car.color = carColors[carcolorindex];
+
+car.speed = randomIntFromInterval(0,100);
+
+cars.push(car);
+}
+
+return cars;
+
+console.log(cars);
+
+
+}
